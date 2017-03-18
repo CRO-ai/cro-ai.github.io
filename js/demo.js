cee4bea40cce729e881e420932314411573836ec
Brady Valentino <bradyvalentino@gmail.com>
2017-03-17 20:36:52 -0700

    Merge CRO from HC Master

---
+++ js/demo.js
@@ -0,0 +1,11 @@
+/**
+ * Particleground demo
+ * @author Jonathan Nicol - @mrjnicol
+ */
+
+document.addEventListener('DOMContentLoaded', function () {
+  particleground(document.getElementById('particles'), {
+    dotColor: '#90B0D5',
+    lineColor: '#90B0D5'
+  });
+}, false);
